e295b9abf2c5fd6d537bc2e6381e49048cbdd74c
Rushabh Shiradkar <rushabhshiradkar1717@gmail.com>
2025-07-23 22:18:28 +0530

    Time: 2 ms (96.67%) | Memory: 42.2 MB (8.28%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) return false; 
+
+                char topChar = stack.peek();
+                if ((ch == ')' && topChar != '(') ||
+                    (ch == '}' && topChar != '{') ||
+                    (ch == ']' && topChar != '[')) {
+                    return false;
+                }
+
+                stack.pop(); 
+            }
+        }
+        return stack.isEmpty();
+    }
+}
