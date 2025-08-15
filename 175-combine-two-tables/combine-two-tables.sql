ea711b7c18b41fc5ee5866d9fe8c340e66ab9591
Rushabh Shiradkar <rushabhshiradkar1717@gmail.com>
2025-08-15 13:18:01 +0530

    Time: 395 ms (65.14%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select firstName,lastname,city,state from Person 
-left join Address on Person.personId = Address.personId 
+select p.firstName, p.lastName, a.city, a.state from Person p
+left join Address a on p.personId = a.personId
